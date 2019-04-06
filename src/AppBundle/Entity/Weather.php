0f57337a6ac2a8ffcfd9c6dd4a510548e50441e4
Wojtek <rybinski05@gmail.com>
2019-04-06 18:05:42 +0200

    Added weather entity

---
+++ src/AppBundle/Entity/Weather.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Weather
+ */
+class Weather
+{
+    /** @var int */
+    private $id;
+
+    /** @var string */
+    private $lat;
+
+    /** @var string */
+    private $lng;
+
+    /** @var int */
+    private $cityId;
+
+    /** @var string */
+    private $cityName;
+
+    /** @var float */
+    private $temp;
+
+    /** @var int */
+    private $pressure;
+
+    /** @var int */
+    private $humidity;
+
+    /** @var float */
+    private $tempMin;
+
+    /** @var float */
+    private $tempMax;
+
+    /** @var float */
+    private $windSpeed;
+
+    /** @var float */
+    private $windDeg;
+
+    /** @var int */
+    private $clouds;
+
+    /** @var int */
+    private $rainOneH;
+
+    /** @var int */
+    private $rainThreeH;
+
+    /** @var int */
+    private $snowOneH;
+
+    /** @var int */
+    private $snowThreeH;
+}
